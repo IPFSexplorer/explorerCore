dd8c1493f3ca8c777d7a5add95dc494f4a1d0fea
Matus <mucka@netsearch.cz>
2020-03-09 12:38:21 +0100

    fix race condition in IPFS connector

--- src/ipfs/IPFSConnector.ts
+++ src/ipfs/IPFSConnector.ts
@@ -1,56 +1,50 @@
 import logger from "../logger";
 import IPFS from "ipfs";
 
-export default class IPFSconnector {
+export default class IPFSconnector
+{
     private static instance: IPFSconnector;
     private static config: object = {};
+    private static _nodeCreatingTask;
     private _node: any;
 
-    static setConfig(config: object) {
+
+    static setConfig(config: object)
+    {
         IPFSconnector.config = config;
     }
 
-    static async getInstanceAsync() {
-        if (!IPFSconnector.instance) {
-            console.log("connectyiong");
+    static async getInstanceAsync()
+    {
+        if (!IPFSconnector.instance)
+        {
+            console.log("connectiong");
             IPFSconnector.instance = new IPFSconnector();
 
-            IPFSconnector.instance._node = await IPFS.create(
+            IPFSconnector._nodeCreatingTask = IPFS.create(
                 IPFSconnector.config
             );
+            IPFSconnector.instance._node = await IPFSconnector._nodeCreatingTask;
             logger.info("node started!");
-
-            // setInterval(async () => {
-            //     try {
-            //         const peers = await IPFSconnector.instance.node.swarm.peers();
-            //         console.log(`The node now has ${peers.length} peers.`);
-            //     } catch (err) {
-            //         console.log("An error occurred trying to check our peers:", err);
-            //     }
-            // }, 2000);
-
-            // // Log out the bandwidth stats every 4 seconds so we can see how our configuration is doing
-            // setInterval(async () => {
-            //     try {
-            //         const stats = await IPFSconnector.instance.node.stats.bw();
-            //         console.log(`\nBandwidth Stats: ${JSON.stringify(stats, null, 2)}\n`);
-            //     } catch (err) {
-            //         console.log("An error occurred trying to check our stats:", err);
-            //     }
-            // }, 4000);
         }
+
+        await IPFSconnector._nodeCreatingTask;
         return IPFSconnector.instance;
     }
 
-    get node(): any {
+    get node(): any
+    {
         return this._node;
     }
 
-    public async shutDownAsync() {
-        try {
+    public async shutDownAsync()
+    {
+        try
+        {
             await this._node.stop();
             logger.info("Node stopped!");
-        } catch (error) {
+        } catch (error)
+        {
             logger.error("Node failed to stop!", error);
         }
     }
