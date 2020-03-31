f45eb329ad4d1fb36dcd01a74a795647f80f8974
Mucka <matus@mucka.sk>
2020-03-31 16:23:17 +0200

    add main exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import Database from "./database/DAL/database/databaseStore";
 import IPFSconnector from "./ipfs/IPFSConnector";
+import Queriable from "./database/DAL/query/queriable";
+import PrimaryKey from "./database/DAL/decorators/primaryKey";
+import Index from "./database/DAL/decorators/Index";
 
 export {
     Database,
-    IPFSconnector
+    IPFSconnector,
+    Queriable,
+    PrimaryKey,
+    Index
 }
